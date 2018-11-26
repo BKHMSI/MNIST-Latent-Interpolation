c1027f610d77fdddc35865da3f42231d2ec8e4ff
balkhamissi <balkhamissi@aucegypt.edu>
2018-11-25 19:55:53 -0500

    bug fixes

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from __future__ import print_function, division
 
 import os
+import sys
 import imageio
 import datetime
 import argparse
 import numpy as np  
 
 from tqdm import tqdm
+from skimage import transform
 
 from keras.datasets import mnist
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
@@ -29,9 +31,11 @@ class MemNet:
         optimizer = Adam(0.0002, 0.5)
 
         # Load the dataset
-        (X_train, self.y_train), (_, _) = mnist.load_data()
+        (X_train, y_train), (_, _) = mnist.load_data()
         X_train = np.expand_dims(X_train, axis=3)
- 
+
+        self.y_train = y_train[:self.memsize]
+        
         # Rescale -1 to 1
         self.X_train = X_train / 127.5 - 1.
 
@@ -164,7 +168,7 @@ class MemNet:
             if (epoch+1) % save_interval == 0:
                 self.generator.save("models/model-{}.h5".format(epoch+1))
 
-    def latent_interpolation(self, model_path, out_path, n_steps, start_num, end_num):
+    def latent_interpolation(self, model_path, out_path, out_name, n_steps, imsize, start_num, end_num):
         
         full_model = load_model(model_path)
         embeddings = full_model.layers[1].get_weights()[0]
@@ -178,40 +182,45 @@ class MemNet:
         vectors = []
         alpha_values = np.linspace(0, 1, n_steps)
 
-        start_idx = np.random.choice(self.y_train==num)
+        start_idx = np.random.choice(np.where(self.y_train==num)[0])
 
         for _ in tqdm(range(abs(start_num-end_num))):
             latent_start = embeddings[start_idx]
             num = num - 1 if countdown else num + 1
-            start_idx = np.random.choice(self.y_train==num)
-            latent_end = embeddings[start_idx] 
+            start_idx = np.random.choice(np.where(self.y_train==num)[0])
+            latent_end = embeddings[start_idx]
             for alpha in alpha_values:
                 vector = latent_start*(1-alpha) + latent_end*alpha
                 vectors.append(vector)
 
         gen_imgs = model.predict(np.array(vectors))
         gen_imgs = np.uint8((0.5 * gen_imgs + 0.5) * 255)
-        imageio.mimsave(os.path.join(out_path, 'latent.gif'), gen_imgs)
+        resized_imgs = np.zeros((gen_imgs.shape[0], imsize, imsize))
+        for i in range(gen_imgs.shape[0]): 
+            resized_imgs[i] = transform.resize(gen_imgs[i].squeeze(), (imsize, imsize))
+        imageio.mimsave(os.path.join(out_path, out_name), resized_imgs)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='MNIST Counter Paramaters')
     # training parameters
     parser.add_argument('-t', '--train',         action="store_true", default=False)
-    parser.add_argument('-e', '--epochs',        type=int, default=1000, help='number of epochs for training')
+    parser.add_argument('-e', '--epochs',        type=int, default=250, help='number of epochs for training')
     parser.add_argument('-b', '--batch-size',    type=int, default=400, help='batch size used in training')
     parser.add_argument('-s', '--save-interval', type=int, default=50, help='save model every interval')
     # generation parameters
     parser.add_argument('-g', '--generate',  action="store_true", default=True)
-    parser.add_argument('-a', '--start-num', type=int, default=9, help='count down/up starts with this number')
-    parser.add_argument('-z', '--end-num',   type=int, default=0, help='count down/up ends with this number')
-    parser.add_argument('-n', '--n-steps',   type=int, default=40, help='number of steps used in latent interpolation')
-    parser.add_argument('-o', '--out-path',  type=str, default="./", help='path to generated gif')
-    parser.add_argument('-m', '--model-path',type=str, default="models/model-1000.h5", help='path to saved model')
+    parser.add_argument('-i', '--imsize',    type=int, default=128,  help='image size of generated gif')
+    parser.add_argument('-a', '--start-num', type=int, default=9,    help='count down/up starts with this number')
+    parser.add_argument('-z', '--end-num',   type=int, default=0,    help='count down/up ends with this number')
+    parser.add_argument('-n', '--n-steps',   type=int, default=30,   help='number of steps used in latent interpolation')
+    parser.add_argument('-o', '--out-path',  type=str, default="output", help='path to generated gif')
+    parser.add_argument('-l', '--out-name',  type=str, default="countdown.gif", help='name of generated gif')
+    parser.add_argument('-m', '--model-path',type=str, default="models/model-250.h5", help='path to saved model')
     args = parser.parse_args()
 
     memnet = MemNet()
     if args.train:
         memnet.train(epochs=args.epochs, batch_size=args.batch_size, save_interval=args.save_interval)
     if args.generate:
-        memnet.latent_interpolation(model_path=args.model_path, out_path=args.out_path, 
-            n_steps=args.n_steps, start_num=args.start_num, end_num=args.end_num)
+        memnet.latent_interpolation(model_path=args.model_path, out_path=args.out_path, out_name=args.out_name, 
+            n_steps=args.n_steps, imsize=args.imsize, start_num=args.start_num, end_num=args.end_num)
